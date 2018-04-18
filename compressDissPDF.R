2bcaba8a9c2126cbf0363293c3c40b36a5962563
Andreas Hill <andreas.hill@usys.ethz.ch>
2018-04-18 17:35:43 +0200

    update 18_04_2018
    update 18_04_2018

---
+++ compressDissPDF.R
@@ -0,0 +1,8 @@
+
+
+library(tools)
+
+Sys.setenv(R_GSCMD="C:\\Program Files\\gs\\gs9.22\\bin\\gswin64.exe")
+# compactPDF('C:/Users/hilla/Desktop/diss_compressed/', gs_quality = "printer") 
+compactPDF('S:/RLP_project_saves/current/Holzvorratschaetzung_RLP/Dissertation/diss_compressed/', gs_quality = "printer") 
+
