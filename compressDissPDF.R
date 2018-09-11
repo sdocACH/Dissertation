50833c59053a4550b56c22e2c2395f1ab3f5c596
Andreas Hill <andreas.hill@usys.ethz.ch>
2018-09-11 21:30:10 +0200

    update_11_09_2018
    update_11_09_2018 (final Diss Version after Defense)

--- compressDissPDF.R
+++ compressDissPDF.R
@@ -4,5 +4,4 @@ library(tools)
 
 Sys.setenv(R_GSCMD="C:\\Program Files\\gs\\gs9.22\\bin\\gswin64.exe")
 # compactPDF('C:/Users/hilla/Desktop/diss_compressed/', gs_quality = "printer") 
-compactPDF('S:/RLP_project_saves/current/Holzvorratschaetzung_RLP/Dissertation/diss_compressed/', gs_quality = "printer") 
-
+compactPDF('D:/Holzvorratschaetzung_RLP/Dissertation/diss_compressed/', gs_quality = "printer") 
